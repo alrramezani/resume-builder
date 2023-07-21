73ef3a259a4d91c95b44a07f0e0dae3dfa777c4a
Alireza <alr.ramezani@gmail.com>
2023-07-21 17:54:03 +0330

    footer in main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,44 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter, Open_Sans,Space_Grotesk } from 'next/font/google'
- 
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter, Open_Sans, Space_Grotesk } from "next/font/google";
+
 const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-inter',
-})
- 
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
+
 const open_sans = Open_Sans({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-open-sans',
-})
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-open-sans",
+});
 const space_grotesk = Space_Grotesk({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-space-grotesk',
-})
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-space-grotesk",
+});
 
 export const metadata: Metadata = {
-  title: 'Resume builder',
-  description: 'free resume builder without regestring and saving any data',
-}
+  title: "Resume builder",
+  description: "free resume builder without regestring and saving any data",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${open_sans.variable} ${space_grotesk.variable}`}>{children}</body>
+      <body
+        className={`${inter.variable} ${open_sans.variable} ${space_grotesk.variable}`}
+      >
+        {children}
+        <footer className=" bg-black py-8 text-white w-full fixed bottom-0 text-sm">
+          <div className=" container px-4">Created with &#9829;</div>
+        </footer>
+      </body>
     </html>
-  )
+  );
 }
